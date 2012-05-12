723dadcf458a2f3caf2f3b121e903f9c6c578434
Philip Greenwood <pegreenw@calpoly.edu>
2012-05-11 18:33:19 -0700

    fixed errors is mCar.py

--- stca/mCar.py
+++ stca/mCar.py
@@ -1,61 +1,72 @@
 import numpy as np
 import random
+import sys
 class Car(object):
+    ### Constructor ###
     def __init__(self,position,velocity,vmax,p,lane_bit_state_ref):
         """initializes"""
-        self.position = position
-        self.velocity = velocity
-        self.vmax = vmax
-        self.p = p
-        self.lane_bit_state_ref = lane_bit_state_ref
-        self.next_car_ref = None
-        self.g = 0
+        self.__position = position
+        self.__velocity = velocity
+        self.__vmax = vmax
+        self.__p = p
+        self.__lane_bit_state_ref = lane_bit_state_ref
+        self.__next_car_ref = None
+        self.__g = 0
         
         
     def print_attributes(self):
         """prints attribultes in object"""
         for attr in self.__dict__:
             print attr,':',getattr(self,attr)
+     
+    ### Private Methods ###        
             
-    def do_slow_randomly(self):
+    def __do_slow_randomly(self):
         """chance of random slowing"""
-        r = random.random()*100
-        if r <= p:
-            self.velocity -= 1
-            
-    def calc_new_g(self):
-        """calculates new distance between obejct and next object"""
-        if self.next_car_ref is not None:
-            self.g = self.next_car_ref.position - self.position - 1
+        r = random.random()
+        if r < p:
+            return True
         else:
-            self.g = len(lane_bit_state_ref) - self.position - 1
+            return False
+            
+    def __calc_new_g(self):
+        """calculates new distance between obejct and next object"""
+        if self.__next_car_ref is not None:
+            self.__g = self.__next_car_ref.__position - self.__position - 1
+        else:
+            self.__g = sys.maxint
+        
+    ### Public Methods ###    
         
     def get_velocity(self):
         """returns velocity of object"""
-        return self.velocity
+        return self.__velocity
     
     def get_position(self):
         """returns position of object"""
-        return self.position
+        return self.__position
     
     def get_next_car(self):
         """returns position of next object"""
-        return self.next_car_ref
+        return self.__next_car_ref
     
     def set_next_car(self,other):           
         """sets the next object to other"""
-        self.next_car_ref = other
+        self.__next_car_ref = other
         
     def calc_new_velocity(self):
         """calculates new velocity"""
-        if self.velocity > self.g:
-            self.velocity = self.g
-        elif self.velocity < self.g and self.velocity < self.vmax:
-            self.velocity += 1
-        if self.velocity > 0:
-            self.do_slow_randomly
+        self.__calc_new_g()
+        if self.__velocity > self.__g:
+            self.__velocity = self.__g
+        elif self.__velocity < self.__g and self.__velocity < self.__vmax:
+            self.__velocity += 1
+        if self.__velocity > 0:
+            if self.__do_slow_randomly == True:
+                self.__velocity -= 1
             
-        
     def move(self):
         """moves object in lane"""
-        self.position = self.position + self.velocity
+        self.__position = self.get_position() + self.get_velocity()
+        if self.__position > len(lane_bit_state_ref):
+            self.__position = self.get_position() - len(lane_bit_state_ref)
