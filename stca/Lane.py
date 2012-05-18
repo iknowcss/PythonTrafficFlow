0254566e8b0e618fd3d27a43bd0e346d63096162
Cody Jenkins <iknowcss@gmail.com>
2012-05-17 21:34:27 -0700

    Finished STCA Lane.py

--- stca/Lane.py
+++ stca/Lane.py
@@ -1,61 +1,81 @@
 from Car import Car
 
 class Lane:
-	# Constructor
+	## Constructor ##
 	def __init__(self,vmax,p,initial_lane_conditions):
+		# Initalize the Lane's instance variables
 		self.__lane_bit_state = [False] * len(initial_lane_conditions)
 		self.__cars = list()
-		
+		self.__step_count = 0
+	
+		# Parse initial_lane_conditions and build and preare the cars and 
+		# car array
+		head_car = None # The car farthest to the left
+		previous_car = None
 		position = 0
-		last_car = None
 		for velocity in initial_lane_conditions:
 			if velocity is None or velocity < 0:
 				self.__lane_bit_state[position] = False
 			else:
 				self.__lane_bit_state[position] = True
-				car = Car(position,velocity,vmax,p,self.__lane_bit_state)
-				if last_car is not None:
-					last_car.set_next_car(car)
-				last_car = car
 				
+				# Ensure the velocities
+				if velocity > vmax:
+					velocity = vmax
+				elif velocity < 0:
+					velocity = 0
+				
+				# Create the new car
+				car = Car(position,velocity,vmax,p,self.__lane_bit_state)
+				
+				# Make this car the head car if there isn't one already
+				if head_car is None:
+					head_car = car
+					
+				# Set the previous car's next_car to the current car
+				if previous_car is not None: 
+					previous_car.set_next_car(car)
+					
+				# Set previous_car to the current car for the next loop runthrough
+				previous_car = car
+			
+			# Increment the iterator
 			position += 1
+		
+		# Now that we're out of the loop, set the last car's next_car reference to
+		# the head car
+		previous_car.set_next_car(head_car)
 	
-	# Private methods
+	## Private methods ##
 	def __calc_new_velocities(self):
-                for car in __cars:
-                        car.calc_new_velocity()
+		for car in self.__cars:
+			car.calc_new_velocity()
 	
 	def __move_cars(self):
-		for car in __cars:
-                        car.move()
-		
-	def __remove_last_car(self):
-		for car in __cars:
-                        car.get__position()
-                        if car.__position == len(__lane_bit_state - 1):    # If car's position is at the last value of the lane bit state, we want to remove it
-                                # The -1 is at the end of __lane_bit_state because I believe
-                                # car.__position starts at 0,1,2,...but len will give the length starting from 1. 
-                                del Lane.__cars[-1]
-                                __lane_bit_state[-1] = False # Now that car is gone so we want to change that value to False
-                                car.set_next_car(None) #I dont think this is going to work like this cause I think this goes forward but we want it to go back a car?
-                                
-                                 
-                        
+		for car in self.__cars:
+			looped = car.move()
+			if looped:
+				self.__loop_last_car()
 	
-	# Public methods
+	def __loop_last_car(self):
+		self.__cars.insert(0,self.__cars.pop())
+		
+	## Public methods ##
 	def step(self):
-		"""TODO: implement Lane#step()"""
+		self.__calc_new_velocities()
+		self.__move_cars()
+		self.__step_count += 1
 		return self.get_lane_bit_state()
 	
 	def get_lane_bit_state(self):
 		return list(self.__lane_bit_state)
 	
 	def get_lane_velocity_state(self):
- 		lcl = []  #lane condition list is empty to start
-		ci = 0   #Specify this so that below we start by pulling the zeroth car from the car list (the first car)
-		for bit_state in __lane_bit_state:
+ 		lcl = [] # Lane condition list is empty to start
+		ci = 0   # Specify this so that below we start by pulling the zeroth car from the car list (the first car)
+		for bit_state in self.__lane_bit_state:
 			if bit_state == True:
-				lcl.append(__cars[ci].get_velocity())  #if true, we add the cars velocity to the list
-				ci += 1  #add so that next time it selects the next car in the list of cars
+				lcl.append(self.__cars[ci].get_velocity()) # If true, we add the cars velocity to the list
+				ci += 1 # Add so that next time it selects the next car in the list of cars
 			else:
-				lcl.append(None)  #if false, we add None
+				lcl.append(None) # If false, we add None
